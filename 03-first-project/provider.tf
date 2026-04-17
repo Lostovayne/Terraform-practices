a95e61da22b350bc29938a83cb51ce75674f4f2d
Deus lo Vult <moises.barrera.dev@gmail.com>
2026-04-17 19:52:02 -0400

    Add S3 bucket resource and random ID suffix generation

--- 03-first-project/provider.tf
+++ 03-first-project/provider.tf
@@ -17,16 +17,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# Genera un sufijo aleatorio para el nombre del bucket
-resource "random_id" "bucket_suffix" {
-  byte_length = 6
-}
-
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-unique-bucket-${random_id.bucket_suffix.hex}"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.example_bucket.bucket
-}
